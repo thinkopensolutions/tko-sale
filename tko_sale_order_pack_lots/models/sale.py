b4082ed7b7426ff30bfaa523f1442610a7aac275
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2018-06-01 23:35:45 -0300

    FIX PR (#18)

--- tko_sale_order_pack_lots/models/sale.py
+++ tko_sale_order_pack_lots/models/sale.py
@@ -26,7 +26,7 @@ class SaleOrder(models.Model):
             assign = any(record.order_line.mapped('pack_id'))
             for line in record.order_line:
                 # break for loop if qty and serial numbers are matched
-                if int(line.product_uom_qty) != len(line.pack_id.pack_ids) or not line.pack_id:
+                if int(line.product_uom_qty) == len(line.pack_id.pack_ids) or not line.pack_id:
                     continue
                 ### unlink lots before linking again
                 line.pack_id.pack_ids.unlink()
